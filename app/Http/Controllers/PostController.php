f74ad9adc60dde5ea31d9de3784d3fe1e39dd79d
ojaq <abangojaq@gmail.com>
2023-03-08 14:21:05 +0700

    custom return api detail post resource

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostDetailResource;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -12,5 +13,10 @@ class PostController extends Controller
         $posts = Post::all();
         // return response()->json(['data' => $posts]);
         return PostResource::collection($posts);
+    }
+
+    public function show($id){
+        $post = Post::findOrFail($id);
+        return new PostDetailResource($post);
     }
 }
