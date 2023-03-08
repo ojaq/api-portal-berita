37ed4333d652152a684d77fad53cb9e2a9ee9bc4
ojaq <abangojaq@gmail.com>
2023-03-08 14:08:46 +0700

    custom return api with resource

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        $posts = Post::all();
+        // return response()->json(['data' => $posts]);
+        return PostResource::collection($posts);
+    }
+}
