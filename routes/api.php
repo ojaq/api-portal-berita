e4adf3e3779e48dda00e5d0df4d2c2d4ed5ebc5a
ojaq <abangojaq@gmail.com>
2023-03-14 14:12:08 +0700

    create access token and login

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']);
 Route::get('/posts2/{id}', [PostController::class, 'show2']);
+
+Route::post('/login', [AuthController::class, 'login']);
