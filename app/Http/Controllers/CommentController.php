24dc2830479e810398808a78b767b337800c366d
ojaq <abangojaq@gmail.com>
2023-03-16 14:14:14 +0700

    added comment on post

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -9,8 +10,14 @@ class CommentController extends Controller
     public function store(Request $request)
     {
         $request -> validate([
-            'post_id' => 'required|exist:posts,id',
-            'comment_content' => 'required',
+            'post_id' => 'required|exists:posts,id',
+            'comments_content' => 'required',
         ]);
+
+        $request['user_id'] = auth()->user()->id;
+
+        $comment = Comment::create($request->all());
+
+        return response()->json($comment);
     }
 }
